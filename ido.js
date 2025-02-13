efd6b98c589b3781410cd44e60c47d52888f411c
Big8695 <dwngmalakvsda@gmail.com>
2025-02-13 16:43:17 +0700

    Add files via upload

---
+++ ido.js
@@ -0,0 +1,5 @@
+function openLetter() {
+    document.querySelector('.flap').style.transform = 'rotateX(180deg)';
+    document.querySelector('.letter').style.transform = 'translateY(-30px)';
+    document.querySelector('.letter').style.opacity = '1';
+}
